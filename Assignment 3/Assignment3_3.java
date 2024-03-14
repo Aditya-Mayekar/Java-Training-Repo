64bffb8e5eb8041f42f77ceec8c44a6386f3dbd5
Aditya Vivek Mayekar <145744562+Aditya-Mayekar@users.noreply.github.com>
2024-03-14 19:47:29 +0530

    Add files via upload

---
+++ Assignment 3/Assignment3_3.java
@@ -0,0 +1,12 @@
+class Assignment3_3{
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = 1; i <= 5; i++){
+			for (int j = 1; j <= 5; j++) {
+				System.out.print(n + " ");
+			}
+			n--;
+			System.out.println();
+		}
+	}
+}
---
+++ Assignment 3/Assignment3_4.java
@@ -0,0 +1,12 @@
+class Assignment3_4{
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++){
+			int n = 10;		
+			for (int j = 1; j <= 10; j++) {
+				System.out.print(n + " ");
+			    n--;
+			}
+			System.out.println();
+		}
+	}
+}
