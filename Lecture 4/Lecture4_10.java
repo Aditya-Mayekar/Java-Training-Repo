d3b607b7a09bc193876ed241229b441cc1348485
Aditya Vivek Mayekar <145744562+Aditya-Mayekar@users.noreply.github.com>
2024-04-06 12:05:06 +0530

    Add files via upload

---
+++ Lecture 4/Lecture4_10.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Lecture4_10{
+	public static void main(String[] args) {
+		System.out.println(new int[]{1, 2, 3, 4, 5, 6}.length);
+		System.out.println(new int[]{0, 3, 9, 1, 2, 4}[2]);
+	}
+}
